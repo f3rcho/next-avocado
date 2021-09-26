5167fe9053c87bbfa5f089e38ba2624386fc91e4
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-09-26 17:51:00 -0300

    Linking pages

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../component/Navbar/Navbar';
 
 const About = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hello from About</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../component/Navbar/Navbar';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hello world from Nextjs</h1>
     </div>
   );
