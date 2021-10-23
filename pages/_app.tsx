eb75c2315c9ebf2eec6f1bfe62c9690d9daca22a
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-10-23 10:01:26 -0300

    reportWebVitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import 'semantic-ui-css/semantic.min.css';
 import '../global.css';
 
 import CartProvider from '@store/Cart';
-
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Providers -- context/providers, theme, data
   // layout
